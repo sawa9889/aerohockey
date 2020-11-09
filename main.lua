ee5f2f5d521f9e05a5ffc3a99c7d7f943da4f863
sawa9889 <cfif.sawa@bk.ru>
2020-11-09 19:58:13 +0300

    Added field

--- main.lua
+++ main.lua
@@ -3,8 +3,12 @@ local HC = require "lib.hardoncollider"
 function love.load()
 	hc = HC.new()
 	players = {}
-	players[1] = hc:circle(100,100,20)
-	players[2] = hc:circle(100,200,20)
+	left_border,right_border,upper_border,lower_border = 50, 400, 50, 500
+	circle_range = 20
+	player1_start = {x = 400, y = 100}
+	player2_start = {x = 400, y = 400}
+	players[1] = hc:circle(player1_start.x, player1_start.y, circle_range)
+	players[2] = hc:circle(player2_start.x, player2_start.y, circle_range)
 end
 
 function love.draw()
@@ -14,16 +18,24 @@ function love.draw()
         shape:draw()
     end
     love.graphics.setColor(1, 1, 1)
+    love.graphics.rectangle('line',
+    						left_border  - circle_range,
+    						upper_border - circle_range,
+    						right_border - left_border  + circle_range,
+    						lower_border - upper_border + circle_range)
+    love.graphics.rectangle('line',
+    						left_border  + right_border-left_border,
+    						upper_border - circle_range ,
+    						right_border - left_border  + circle_range,
+    						lower_border - upper_border + circle_range)
 end
 
 function love.update(dt)
 	local x,y = love.mouse.getPosition()
 	local cx,cy = players[1]:center()
-	local left_border,right_border,upper_border,lower_border = 100, 800, 100, 800
 	if (x > right_border or x < left_border) or (y > lower_border or y < upper_border)then
 		x = cx
 		y = cy
-
 	end
 
 	players[1]:moveTo(x, y)
