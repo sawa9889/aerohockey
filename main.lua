d0a3561e6988d9e97d4b9bc115b7a627213ae973
savior <saviorium@gmail.com>
2020-11-10 00:09:47 +0300

    Add player shadows

--- main.lua
+++ main.lua
@@ -6,13 +6,13 @@ function love.load()
 	hc = HC.new()
 	players = {}
 	left_border,right_border,upper_border,lower_border = 50, 400, 50, 500
-	circle_range = 20
+	circle_range = 40
 	player1_start = {x = right_border/2 , y = lower_border/2}
 	player2_start = {x = right_border*3/2 , y = lower_border/2}
 	players[1] = hc:circle(player1_start.x, player1_start.y, circle_range)
 	players[2] = hc:circle(player2_start.x, player2_start.y, circle_range)
 
-	ball_range = 10
+	ball_range = 15
 	ball_start = {x = right_border, y = lower_border/2}
 	ball = {shape = hc:circle(ball_start.x, ball_start.y, ball_range), vector = Vector(0, 0) }
 	ball.shape.type = 'ball'
@@ -66,13 +66,15 @@ function love.update(dt)
 	players[1]:moveTo(x, y)
 	ball.vector = ball.vector*1
 	local isCollided = false
-	local index = 0
-	local max_index = 5
+	local index = 1
+	local max_index = 10
 	local cx, cy = ball.shape:center()
+	local pcx, pcy = players[1]:center()
+	local dx = pcx - x
+	local dy = pcy - y
 	while not isCollided and index <= max_index do
 		ball_shadow:moveTo(cx + ball.vector.x * index  / max_index, cy + ball.vector.y * index  / max_index)
-		local pcx, pcy = players[1]:center()
-		players[1]:move((x - pcx) * index / max_index , (y - pcy) * index / max_index)
+		players[1]:moveTo(pcx + dx * index / max_index , pcy + dy * index / max_index)
 	    for shape, delta in pairs(hc:collisions(ball_shadow)) do
 	    	if shape.type ~= 'ball' then
 		        ball.vector = ball.vector + delta
